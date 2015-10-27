1cda9ecccfd214db4b5b167f9e1e97a9dff8d99b
TonyH <h@realh.co.uk>
2015-10-27 14:26:05 +0000

    Refactored shader sources.

--- tools/mkshaderregs.py
+++ tools/mkshaderregs.py
@@ -1,98 +1,141 @@
 #!/usr/bin/env python3
 
+# Copyright 2015 Tony Houghton, h@realh.co.uk
+# 
+# This file is part of the realh fork of the Parallax project.
+# 
+# Parallax is free software: you can redistribute it and/or modify it 
+# under the terms of the Creative Commons Attribution 3.0 Unported License.
+# 
+# Parallax is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution 
+# 3.0 Unported License. for more details.
+# 
+# You should have received a copy of the the Creative Commons Attribution 
+# 3.0 Unported License along with Parallax. 
+# If not, see http://creativecommons.org/licenses/by/3.0/.
+
 import os, sys
 
-shaders_dir = os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])),
-        'src', 'thothbot', 'parallax', 'core', 'client', 'shaders')
+TEMPLATE_BOTH = \
+"""// Generated from %s and %s
 
-HEADER = """/*
- * Copyright 2015 Tony Houghton, h@realh.co.uk
- * 
- * This file is part of the realh fork of the Parallax project.
- * 
- * Parallax is free software: you can redistribute it and/or modify it 
- * under the terms of the Creative Commons Attribution 3.0 Unported License.
- * 
- * Parallax is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution 
- * 3.0 Unported License. for more details.
- * 
- * You should have received a copy of the the Creative Commons Attribution 
- * 3.0 Unported License along with Parallax. 
- * If not, see http://creativecommons.org/licenses/by/3.0/.
- */
-
-package thothbot.parallax.core.client.shaders;
-
-import java.util.HashMap;
+package thothbot.parallax.core.client.shaders.%s;
 
 public class %s
 {
-    private static HashMap<String, String> shaderMap;
+    public static final String vertex =
+%s;
 
-    public static void set(String k, String v)
-    {
-        if (shaderMap == null)
-            shaderMap = new HashMap<String, String>();
-        shaderMap.put(k, v);
-    }
+    public static final String fragment =
+%s;
+}
+"""
 
-    public static String get(String k)
-    {
-        return shaderMap.get(k);
-    }
+TEMPLATE_VERT = \
+"""// Generated from %s
 
-    static
-    {"""
+package thothbot.parallax.core.client.shaders.%s;
 
-class ShaderSourceRegistry(object):
+public class %s
+{
+    public static final String vertex =
+%s;
+}
+"""
 
-    def __init__(self, name):
-        global HEADER
-        self.name = name
-        self.java = HEADER % name
+TEMPLATE_FRAG = \
+"""// Generated from %s
 
-    def close_and_save(self):
-        global shaders_dir
-        self.java += "\n    }\n}"
-        fp = open(os.path.join(shaders_dir, self.name + '.java'), 'w')
-        fp.write(self.java)
-        fp.close()
+package thothbot.parallax.core.client.shaders.%s;
 
-    def add_shader(self, name, source):
-        source = '"' + source.strip().replace('\n', '\\n" +\n"') + '\\n"'
-        self.java += '\n\n        set("%s",\n%s);' % (name, source)
+public class %s
+{
+    public static final String fragment =
+%s;
+}
+"""
+
+opj = os.path.join
 
 
-vert_chunks = ShaderSourceRegistry('VertexShaderChunks')
-frag_chunks = ShaderSourceRegistry('FragmentShaderChunks')
-vert_sources = ShaderSourceRegistry('VertexShaderSources')
-frag_sources = ShaderSourceRegistry('FragmentShaderSources')
+dest_dir = opj(os.path.dirname(os.path.dirname(sys.argv[0])),
+        'src', 'thothbot', 'parallax', 'core', 'client', 'shaders')
+src_dir = opj(os.path.dirname(os.path.dirname(sys.argv[0])), 'shaders')
+
+
+def make_jstring(dir_name, src_name):
+    src_name = opj(dir_name, src_name)
+    if not os.path.exists(src_name):
+        return None
+    fp = open(src_name, 'r')
+    source = fp.read()
+    fp.close()
+    return '"' + source.strip().replace('\n', '\\n" +\n"') + '\\n"'
+
+
+def glsl_to_java(src):
+    global TEMPLATE, dest_dir, src_dir
+
+    is_frag = False
+
+    if src.endswith('.vs'):
+        vert_src = src
+        basename = src[:-3]
+        frag_src = basename + '.fs'
+        subdir = 'source'
+    elif src.endswith('_vertex.glsl'):
+        vert_src = src
+        basename = src[:-12]
+        frag_src = basename + '_fragment.glsl'
+        subdir = 'chunk'
+    elif src.endswith('.fs'):
+        frag_src = src
+        basename = src[:-3]
+        vert_src = basename + '.vs'
+        subdir = 'source'
+        is_frag = True
+    elif src.endswith('_fragment.glsl'):
+        frag_src = src
+        basename = src[:-14]
+        vert_src = basename + '_vertex.glsl'
+        subdir = 'chunk'
+        is_frag = True
+    else:
+        raise Exception('Unrecognised filename "%s"' % src)
+
+    src_d = opj(src_dir, subdir)
+    dst_d = opj(dest_dir, subdir)
+
+    # Can't have keyword as class name
+    if basename == 'default':
+        basename = 'default_shader'
+
+    vert_jstring = make_jstring(src_d, vert_src)
+    frag_jstring = make_jstring(src_d, frag_src)
+    if is_frag:
+        if vert_jstring:
+            # Both exist, leave it to when src is for vertex
+            return
+        java = TEMPLATE_FRAG % (frag_src, subdir, basename, frag_jstring)
+    elif frag_jstring:
+        java = TEMPLATE_BOTH % (vert_src, frag_src, subdir, basename,
+                vert_jstring, frag_jstring)
+    else:
+        java = TEMPLATE_VERT % (vert_src, subdir, basename, vert_jstring)
+    
+    fp = open(opj(dst_d, basename + '.java'), 'w')
+    fp.write(java)
+    fp.close()
+
 
 
 def scan_directory(dirname):
-    global vert_chunks, frag_chunks, vert_sources, frag_sources
     files = os.listdir(dirname)
     for f in files:
-        fp = open(os.path.join(dirname, f), 'r')
-        source = fp.read()
-        fp.close()
-        if f.endswith('_fragment.glsl'):
-            frag_chunks.add_shader(f[:-14], source)
-        elif f.endswith('_vertex.glsl'):
-            vert_chunks.add_shader(f[:-12], source)
-        elif f.endswith('.fs'):
-            frag_sources.add_shader(f[:-3], source)
-        elif f.endswith('.vs'):
-            vert_sources.add_shader(f[:-3], source)
+        glsl_to_java(f)
 
 
-scan_directory(os.path.join(shaders_dir, 'chunk'))
-scan_directory(os.path.join(shaders_dir, 'source'))
-
-vert_chunks.close_and_save()
-frag_chunks.close_and_save()
-vert_sources.close_and_save()
-frag_sources.close_and_save()
-
+scan_directory(os.path.join(src_dir, 'chunk'))
+scan_directory(os.path.join(src_dir, 'source'))
