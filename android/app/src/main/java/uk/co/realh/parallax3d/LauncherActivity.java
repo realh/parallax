f8dadc445dba8a64dda6634e0cd94f40ecf43f38
TonyH <h@realh.co.uk>
2015-11-04 17:15:14 +0000

    Added LauncherActivity.

---
+++ android/app/src/main/java/uk/co/realh/parallax3d/LauncherActivity.java
@@ -0,0 +1,24 @@
+package uk.co.realh.parallax3d;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import realh.co.uk.parallax3d.R;
+
+public class LauncherActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+    public void onCubeTextureClick(View view)
+    {
+        startActivity(new Intent(this, CubeTextureActivity.class));
+    }
+}
