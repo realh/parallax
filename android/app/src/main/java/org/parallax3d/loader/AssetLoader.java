b49c98535be769ed9580c4255856c02db4cce6b0
TonyH <h@realh.co.uk>
2015-11-19 16:28:00 +0000

    Added stream loaders to AssetLoader.

--- android/app/src/main/java/org/parallax3d/loader/AssetLoader.java
+++ android/app/src/main/java/org/parallax3d/loader/AssetLoader.java
@@ -18,11 +18,18 @@
 
 package org.parallax3d.loader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 /**
  * Loads assets from a certain directory.
  */
 public class AssetLoader
 {
+	private static final int CHUNK_SIZE = 8192;
+
 	protected String dirName;
 
 	/**
@@ -47,5 +54,141 @@ public class AssetLoader
 	public String getPathname(String leafName)
 	{
 		return dirName + "/" + leafName;
+	}
+
+	/**
+	 * Note this does not close the stream.
+	 * @param strm
+	 * @return
+	 * @throws IOException
+	 */
+	public byte[] loadData(InputStream strm) throws IOException
+	{
+		// There isn't a reliable way to find the length of a stream,
+		// so just keep reading until we get -1.
+		byte[] buf = null;
+		int offset = 0;
+
+        int numRead = 0;
+
+		do
+		{
+			// At least we can try to make a good guess at size
+			int available = strm.available();
+			if (available < CHUNK_SIZE)
+				available = CHUNK_SIZE;
+			if (buf == null)
+			{
+				buf = new byte[available];
+			} else if (available <= buf.length - offset)
+			{
+				available = buf.length - offset;
+			} else
+			{
+				byte[] oldBuf = buf;
+
+				available -= oldBuf.length - offset;
+				if (available < CHUNK_SIZE)
+					available = CHUNK_SIZE;
+				offset = oldBuf.length;
+				buf = new byte[offset + available];
+				System.arraycopy(oldBuf, 0, buf, 0, offset);
+			}
+			numRead = strm.read(buf, offset, available);
+			if (numRead != -1)
+				offset += numRead;
+		} while (numRead != -1);
+
+		if (offset < buf.length)
+		{
+			buf = Arrays.copyOf(buf, offset);
+		}
+
+		return buf;
+	}
+
+	/*
+	 * This is the most efficient way I can think of to load a String with
+	 * the API available.
+	 * Note this closes the stream when done.
+	 */
+	public String loadText(InputStream strm) throws IOException
+	{
+		InputStreamReader reader = null;
+		StringBuilder builder = null;
+		String result = null;
+
+		try
+		{
+			reader = new InputStreamReader(strm);
+
+			int numRead = 0;
+
+			char[] buf = null;
+			int bufLen = 0;
+
+			do
+			{
+				// If we're lucky available() might return the total size
+				// of the file
+				int available = strm.available();
+
+				//Log.debug("" + available + " bytes available");
+
+				if (available < CHUNK_SIZE)
+					available = CHUNK_SIZE;
+
+				// Create new buffer if necessary
+				if (buf == null)
+				{
+					buf = new char[bufLen = available];
+				}
+
+				// Try to fill the buffer
+				numRead = reader.read(buf);
+
+				//Log.debug("Read " + numRead + " chars");
+
+				if (numRead > 0)
+				{
+					// We've read something, make/add to string
+					if (builder == null)
+					{
+						if (result == null)
+						{
+							// On first pass make a plain String, we can return
+							// this without making a StringBuilder if there is
+							// no second pass
+							result = new String(buf, 0, numRead);
+						}
+						else
+						{
+							// This is second pass, make a StringBuilder with
+							// contents of first pass, then current and
+							// further passes will be added to the builder
+							builder = new StringBuilder(result);
+							result = null;
+						}
+					}
+					// If we already had a builder or a new one was just
+					// created we need to add the just-read buffer to it
+					if (builder != null)
+						builder.append(buf, 0, numRead);
+				}
+			} while (numRead != -1);
+		} finally
+		{
+			if (reader != null)
+				reader.close();
+			else
+				strm.close();
+		}
+
+		if (result == null && builder != null)
+		{
+			builder.trimToSize();
+			result = builder.toString();
+		}
+		return result;
 	}
 }
