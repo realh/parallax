7222bd10436bcd4d522d9157ef32e6865c558bea
TonyH <h@realh.co.uk>
2015-11-21 17:19:15 +0000

    Restored Android folder.

---
+++ src/org/parallax3d/android/AndroidAssetLoader.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Tony Houghton, h@realh.co.uk
+ *
+ * This file is part of the Android port of the Parallax project.
+ * 
+ * Parallax is free software: you can redistribute it and/or modify it 
+ * under the terms of the Creative Commons Attribution 3.0 Unported License.
+ * 
+ * Parallax is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution 
+ * 3.0 Unported License. for more details.
+ * 
+ * You should have received a copy of the the Creative Commons Attribution 
+ * 3.0 Unported License along with Parallax. 
+ * If not, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+package org.parallax3d.android;
+
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AndroidAssetLoader extends AndroidLoader
+{
+
+	protected AssetManager assets;
+
+	/**
+	 * @param dirName Use a trailing / if this is a directory, otherwise
+	 *                the directory containing the named file is used.
+	 */
+	public AndroidAssetLoader(AssetManager assets, String dirName)
+	{
+		super(dirName);
+		this.assets = assets;
+	}
+
+	@Override
+	public InputStream openInputStream(String filename) throws IOException
+	{
+		if (filename.charAt(0) != '/')
+			filename = getPathname(filename);
+		return assets.open(filename);
+	}
+}
